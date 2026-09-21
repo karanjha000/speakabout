365fe5da5415db558e65e3776aaa051ae935cd2b
karan jha <Karanjhax12@gmail.com>
2026-09-21 09:42:42 +0530

    chore: initialize Spring Boot project with Spring Web dependency

---
+++ src/main/java/com/speakabout/core/SpeakaboutApplication.java
@@ -0,0 +1,13 @@
+package com.speakabout.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakaboutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeakaboutApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/speakabout/core/controller/CommandController.java
@@ -0,0 +1,15 @@
+package com.speakabout.core.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/command")
+@RestController
+public class CommandController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "Backend is live";
+    }
+}
